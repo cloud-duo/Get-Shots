63b3ee0b940e3a484735be3188f3f966c921aca4
D-Andreea <das.andreea@gmail.com>
2019-04-04 23:06:59 +0300

    Refactoring

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import tempfile
 
 import cv2
 import flask
-from flask import Flask
+from flask import Flask, jsonify
 from google.cloud import storage
 import pymysql
-import images
 from google.cloud import videointelligence
 
 
@@ -23,79 +22,42 @@ def get_shots(video_id):
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(video_id + '.mp4')
 
-    # destination_file_name = os.path.join('Bucket', video_id + '.mp4')
-    # blob.download_to_file()
-
-
-    #fd, path = tempfile.mkstemp()
     with tempfile.NamedTemporaryFile() as temp_video:
         blob.download_to_file(temp_video)
 
         shots = analyze_shots(video_id + '.mp4')
-        #images.extract(video_id, shots, temp_video)
 
-####################################################################################################
-        # temp_filename = os.path.join(tempfile.gettempdir(), temp_video.name)
-        # local_temp_file = open(temp_filename, mode='w')
-        # temp_video.seek(0)
-        # local_temp_file.write(temp_video.read())
-        # local_temp_file.close()
         temp_video.seek(0)
         cam = cv2.VideoCapture(temp_video.name)
+
         currentframe = 0
-        needed_frames = images.get_frames_number(shots)
-        counter = 0
-        print('Needed frames: ', needed_frames)
+        uploaded_image_counter = 0
+
         storage_client = storage.Client.from_service_account_json('keys.json')
         bucket_name = 'galeata_magica_123'
         bucket = storage_client.get_bucket(bucket_name)
 
         frames_per_second = cam.get(cv2.CAP_PROP_FPS)
-
-        for i in range(0, len(needed_frames)):
-            needed_frames[i] = int(needed_frames[i] * frames_per_second)
+        needed_frames = get_frames_number(shots, frames_per_second)
 
         while True:
-            # reading from frame
             ret, frame = cam.read()
-            # print('---------- ', ret, frame)
             if ret:
                 if currentframe in needed_frames:
-                    with tempfile.NamedTemporaryFile() as gcs_image:
-                        iName = "".join([str(gcs_image.name), ".jpg"])
-                        # save image to temp file
-                        cv2.imwrite(iName, frame)
-                        # frame.tofile(gcs_image)
-                        # gcs_image.seek(0)
-                        # data = cv2.imencode('.jpg', gcs_image)[1].tostring()
-                        image_blob = bucket.blob(video_id + '/' + str(counter) + '.jpg')
-                        image_blob.upload_from_filename(iName)
-
-                        # with NamedTemporaryFile() as temp:
-                        #     # Extract name to the temp file
-                        #     iName = "".join([str(temp.name), ".jpg"])
-                        #     # save image to temp file
-                        #     cv2.imwrite(iName, duplicate_image)
-
-
-                    # increasing counter so that it will
-                    # show how many frames are created
-                    print('Counter: ', counter)
-                    counter += 1
-                print('Counter: ', currentframe)
+                    with tempfile.NamedTemporaryFile() as temporary_image:
+                        path_temp_image = "".join([str(temporary_image.name), ".jpg"])
+                        cv2.imwrite(path_temp_image, frame)
+                        image_blob = bucket.blob(video_id + '/' + str(uploaded_image_counter) + '.jpg')
+                        image_blob.upload_from_filename(path_temp_image)
+                    uploaded_image_counter += 1
                 currentframe += 1
-
             else:
                 break
-        # Release all space and windows once done
         cam.release()
         cv2.destroyAllWindows()
-####################################################################################################
-    #os.remove(path)
 
-    resp = flask.Response("200")
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+    uploaded_image_counter += 1
+    return jsonify({"Counter": uploaded_image_counter})
 
 
 def analyze_shots(path):
@@ -125,6 +87,15 @@ def after_request(response):
     header = response.headers
     header['Access-Control-Allow-Origin'] = '*'
     return response
+
+
+def get_frames_number(shots, fps):
+    frames = list()
+    for pair in shots:
+        frames.append(int((pair[0] + pair[1])/2))
+    for i in range(0, len(frames)):
+        frames[i] = int(frames[i] * fps)
+    return frames
 
 
 if __name__ == '__main__':
